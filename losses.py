ea51977813f6217616f7e719593551c3aee7c180
postBG <profile2697@gmail.com>
2019-04-16 17:51:46 +0900

    fix dice loss to match interface

--- losses.py
+++ losses.py
@@ -8,13 +8,13 @@ def get_weights_as_tensor(class_weight, device):
     return torch.Tensor(CLASS_WEIGHT[class_weight]).to(device)
 
 
-def dice_loss(true, logits, eps=1e-7):
+def dice_loss(logits, targets, eps=1e-7):
     """Computes the Sørensen–Dice loss.
     Note that PyTorch optimizers minimize a loss. In this
     case, we would like to maximize the dice loss so we
     return the negated dice loss.
     Args:
-        true: a tensor of shape [B, 1, H, W].
+        targets: a tensor of shape [B, 1, H, W].
         logits: a tensor of shape [B, C, H, W]. Corresponds to
             the raw output or logits of the model.
         eps: added to the denominator for numerical stability.
@@ -24,9 +24,10 @@ def dice_loss(true, logits, eps=1e-7):
 
     FROM https://github.com/kevinzakka/pytorch-goodies/blob/master/losses.py
     """
+    # TODO: remove hard-coded numbers.
     num_classes = logits.shape[1]
     if num_classes == 1:
-        true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
+        true_1_hot = torch.eye(num_classes + 1)[targets.squeeze(1)]
         true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
         true_1_hot_f = true_1_hot[:, 0:1, :, :]
         true_1_hot_s = true_1_hot[:, 1:2, :, :]
@@ -35,15 +36,15 @@ def dice_loss(true, logits, eps=1e-7):
         neg_prob = 1 - pos_prob
         probas = torch.cat([pos_prob, neg_prob], dim=1)
     else:
-        true_1_hot = torch.eye(256)[true.squeeze(1)]
+        true_1_hot = torch.eye(256)[targets.squeeze(1)]
         true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
         probas = F.softmax(logits, dim=1)
     true_1_hot = true_1_hot.type(logits.type())
-    dims = (0,) + tuple(range(2, true.ndimension()))
+    dims = (0,) + tuple(range(2, targets.ndimension()))
     intersection = torch.sum(probas * true_1_hot[:, :25, :, :], dims)
     cardinality = torch.sum(probas + true_1_hot[:, :25, :, :], dims)
     dice_loss = (2. * intersection / (cardinality + eps)).mean()
-    return (1 - dice_loss)
+    return 1 - dice_loss
 
 
 def create_criterion(args):
