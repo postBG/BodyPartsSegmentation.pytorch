168d8ec877607f3bfb05a076a11b1f079f081074
postBG <profile2697@gmail.com>
2019-04-16 19:36:05 +0900

    remove hard coded numbers

--- losses.py
+++ losses.py
@@ -8,41 +8,33 @@ def get_weights_as_tensor(class_weight, device):
     return torch.Tensor(CLASS_WEIGHT[class_weight]).to(device)
 
 
-def dice_loss(logits, targets, eps=1e-7):
+def dice_loss(logits, targets, eps=1e-7, ignore_label=IGNORE_LABEL):
     """Computes the Sørensen–Dice loss.
     Note that PyTorch optimizers minimize a loss. In this
     case, we would like to maximize the dice loss so we
     return the negated dice loss.
     Args:
-        targets: a tensor of shape [B, 1, H, W].
         logits: a tensor of shape [B, C, H, W]. Corresponds to
-            the raw output or logits of the model.
+            the raw output or logits of the model. (C should be bigger than 1)
+        targets: a tensor of shape [B, 1, H, W].
         eps: added to the denominator for numerical stability.
+        ignore_label: Default = 255
     Returns:
         dice_loss: the Sørensen–Dice loss.
 
 
     FROM https://github.com/kevinzakka/pytorch-goodies/blob/master/losses.py
     """
-    # TODO: remove hard-coded numbers.
     num_classes = logits.shape[1]
-    if num_classes == 1:
-        true_1_hot = torch.eye(num_classes + 1)[targets.squeeze(1)]
-        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
-        true_1_hot_f = true_1_hot[:, 0:1, :, :]
-        true_1_hot_s = true_1_hot[:, 1:2, :, :]
-        true_1_hot = torch.cat([true_1_hot_s, true_1_hot_f], dim=1)
-        pos_prob = torch.sigmoid(logits)
-        neg_prob = 1 - pos_prob
-        probas = torch.cat([pos_prob, neg_prob], dim=1)
-    else:
-        true_1_hot = torch.eye(256)[targets.squeeze(1)]
-        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
-        probas = F.softmax(logits, dim=1)
-    true_1_hot = true_1_hot.type(logits.type())
+
+    one_hot_targets = torch.eye(ignore_label + 1)[targets.squeeze(1)]
+    one_hot_targets = one_hot_targets.permute(0, 3, 1, 2).float()
+    predictions = F.softmax(logits, dim=1)
+
+    one_hot_targets = one_hot_targets.type(logits.type())
     dims = (0,) + tuple(range(2, targets.ndimension()))
-    intersection = torch.sum(probas * true_1_hot[:, :25, :, :], dims)
-    cardinality = torch.sum(probas + true_1_hot[:, :25, :, :], dims)
+    intersection = torch.sum(predictions * one_hot_targets[:, :num_classes, :, :], dims)
+    cardinality = torch.sum(predictions + one_hot_targets[:, :num_classes, :, :], dims)
     dice_loss = (2. * intersection / (cardinality + eps)).mean()
     return 1 - dice_loss
 
