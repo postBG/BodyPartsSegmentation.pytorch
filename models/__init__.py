b72d406f451614bcf498086738ff2155c142c516
hongsukchoi <redstonepo@gmail.com>
2019-04-29 16:00:58 +0900

    add new xception
    a

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,11 @@
 from models.deeplab_v3 import DeepLab
-from models.fcn import ResFCN
-from models.new_deep import DeepLabv3_plus
+# from models.fcn import ResFCN
 
 
 def model_factory(args):
-    # test
-    #return DeepLabv3_plus(nInputChannels=3, n_classes=25, os=16, pretrained=True, freeze_bn=False, _print=True)
-
     if args.seg_model == 'deeplab_v3':
         return DeepLab(backbone=args.backbone, output_stride=16, num_classes=25, freeze_bn=False)
-    elif args.seg_model == 'fcn':
-        return ResFCN(num_classes=25)
+    # elif args.seg_model == 'fcn':
+    #     return ResFCN(num_classes=25)
     else:
         raise ValueError("{} is not supported.".format(args.seg_model))
--- models/backbone/__init__.py
+++ models/backbone/__init__.py
@@ -1,9 +1,10 @@
 from models.backbone.Xception import AlignedXception
+from models.backbone.Xception2 import get_xception
 
 
 def get_backbone(name, output_stride, BatchNorm=None, pretrained=True):
     backbone = None
     if name == 'xception':
-        backbone = AlignedXception(output_stride, BatchNorm, pretrained=pretrained)
+        backbone = get_xception(pretrained=True) #AlignedXception(output_stride, BatchNorm, pretrained=pretrained)
 
     return backbone
