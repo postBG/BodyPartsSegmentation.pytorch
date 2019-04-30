4fac0528033b109680ea7553d3c4a5508502c444
hyBlue <gusdud1500@gmail.com>
2019-05-01 00:40:40 +0900

    add resnet backbone

--- models/backbone/__init__.py
+++ models/backbone/__init__.py
@@ -1,10 +1,13 @@
 from models.backbone.Xception import AlignedXception
 from models.backbone.Xception2 import get_xception
-
+from models.backbone import resnet
 
 def get_backbone(name, output_stride, BatchNorm=None, pretrained=True):
     backbone = None
     if name == 'xception':
         backbone = get_xception(pretrained=True) #AlignedXception(output_stride, BatchNorm, pretrained=pretrained)
+
+    elif name == 'resent':
+        return resnet.ResNet101(output_stride, BatchNorm)
 
     return backbone
