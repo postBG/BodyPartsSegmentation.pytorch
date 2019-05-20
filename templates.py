a94d480112c74dac6e2267a35af8617117a9f84c
postBG <profile2697@gmail.com>
2019-05-21 02:21:20 +0900

    update templates

--- templates.py
+++ templates.py
@@ -28,7 +28,7 @@ def set_template(args):
         args.decay_step = 5
         args.gamma = 0.1
         args.weight_decay = 2e-5
-        args.transform_type = 'none'
+        args.train_transform_type = 'none'
         args.experiment_description = 'with_proximal_log_weighted_ce'
         args.criterion = 'ce'
         args.class_weight = 'proximal_log'
@@ -49,19 +49,21 @@ def set_template(args):
         args.criterion = 'ce'
 
     elif args.template == 'dice':
+        args.resume_training = 'deeplab-resnet.pth.tar'
         args.backbone = 'resnet'
         args.test = False
         args.batch_size = 4
-        args.lr = 0.007
+        args.lr = 0.00001
         args.epoch = 120
         args.optimizer = 'SGD'
         args.momentum = 0.9
         args.decay_step = 5
         args.gamma = 0.1
         args.weight_decay = 2e-5
-        args.train_transform_type = 'none'
-        args.experiment_description = 'with_dice_loss'
+        args.train_transform_type = 'random'
+        args.experiment_description = 'with_pretrained_deeplab'
         args.criterion = 'dice'
+        args.classes = 25
 
     elif args.template == 'merged':
         args.resume_training = 'with_merged_annotations_2019-05-18_0'
@@ -75,7 +77,7 @@ def set_template(args):
         args.decay_step = 5
         args.gamma = 0.1
         args.weight_decay = 2e-5
-        args.transform_type = 'none'
+        args.train_transform_type = 'random'
         args.experiment_description = 'with_merged_annotations_after'
         args.criterion = 'ce'
         args.classes = 25
@@ -93,7 +95,7 @@ def set_template(args):
         args.decay_step = 5
         args.gamma = 0.1
         args.weight_decay = 2e-5
-        args.transform_type = 'none'
+        args.train_transform_type = 'random'
         args.experiment_description = 'with_pretrained_deeplab'
         args.criterion = 'ce'
         args.classes = 25
