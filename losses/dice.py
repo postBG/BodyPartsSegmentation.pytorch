76af4bbec50cdd667024290e52c0ddeb5c2939d4
postBG <profile2697@gmail.com>
2019-05-24 01:15:29 +0900

    refactor dice

--- losses/dice.py
+++ losses/dice.py
@@ -3,11 +3,11 @@ from torch.nn import functional as F
 
 
 class DiceLoss(object):
-    def __init__(self, device, eps=1e-7, ignore_index=None, use_weights=False):
+    def __init__(self, device, eps=1e-7, ignore_index=None, weights_name='none'):
         self.device = device
         self.eps = eps
         self.ONE_HOT_EMBEDDING = torch.eye(ignore_index + 1)
-        self.use_weights = use_weights
+        self.weights_name = weights_name
 
     def __call__(self, logits, targets):
         """
@@ -19,7 +19,7 @@ class DiceLoss(object):
                 dice_loss: the Sørensen–Dice loss.
             """
         num_classes = logits.shape[1]
-        weights = self._calculate_weights(targets, num_classes, self.use_weights)
+        weights = self._calculate_weights(targets, num_classes, self.weights_name)
 
         one_hot_targets = self.ONE_HOT_EMBEDDING[targets.squeeze(1)]
         one_hot_targets = one_hot_targets.permute(0, 3, 1, 2)[:, :num_classes, :, :]
@@ -33,9 +33,12 @@ class DiceLoss(object):
         loss = 1 - 2 * (intersection + self.eps) / (cardinality + self.eps)
         return loss
 
-    def _calculate_weights(self, targets, num_classes, use_weights):
-        if not use_weights:
+    def _calculate_weights(self, targets, num_classes, weights_name):
+        if weights_name == 'none':
             return torch.Tensor([1. for _ in range(num_classes)]).to(self.device)
+        elif weights_name == 'general':
+            num_pixels = torch.Tensor([torch.sum(targets == label) for label in range(num_classes)]).to(self.device)
+            return 1 / (num_pixels * num_pixels + self.eps)
+        else:
+            raise ValueError("There's no {} weights.".format(weights_name))
 
-        num_pixels = torch.Tensor([torch.sum(targets == label) for label in range(num_classes)]).to(self.device)
-        return 1 / (num_pixels * num_pixels + self.eps)
